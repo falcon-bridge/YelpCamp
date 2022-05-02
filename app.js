e8e0ff6ab9f33fc61ad78fcbbd2fea5616751d8d
Aayush Kumar <falcon.bridge2000@gmail.com>
2022-05-02 10:28:01 +0530

    configured sessions

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 
 const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
+const session = require("express-session");
 
 const ExpressError = require("./utils/ExpressError");
 
@@ -14,7 +15,6 @@ const reviews = require("./routes/reviews");
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false,
 });
 
 const db = mongoose.connection;
@@ -28,6 +28,13 @@ const app = express();
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+const sessionConfig = {
+  secret: "thisshouldbeabettersecret!",
+  resave: false,
+  saveUninitialized: true,
+};
+app.use(session(sessionConfig));
 
 //for parsing the request body
 app.use(express.urlencoded({ extended: true }));
