6cd92cd49ecd71210630a448b5469af1ac514300
Aayush Kumar <falcon.bridge2000@gmail.com>
2022-05-03 17:59:03 +0530

    made the route for registraion of new user

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,28 @@
 const express = require("express");
+
+const User = require("../models/user");
+const catchAsync = require("../utils/catchAsync");
+
 const router = express.Router();
 
 router.get("/register", (req, res) => {
   res.render("users/register");
 });
+
+router.post(
+  "/register",
+  catchAsync(async (req, res, next) => {
+    try {
+      const { email, username, password } = req.body;
+      const user = new User({ email, username });
+      const registeredUser = await User.register(user, password);
+      req.flash("success", "Welcome to Yelp Camp!");
+      res.redirect("/campgrounds");
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("register");
+    }
+  })
+);
 
 module.exports = router;
