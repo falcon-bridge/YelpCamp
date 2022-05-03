08b567b4db5459796e7b25e41c950cf0e492193a
Aayush Kumar <falcon.bridge2000@gmail.com>
2022-05-03 17:25:54 +0530

    configured passport

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+
+const User = require("./models/user");
 
 const ExpressError = require("./utils/ExpressError");
 
@@ -47,6 +51,13 @@ app.use(flash());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
