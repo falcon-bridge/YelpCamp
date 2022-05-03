f05d7c520de225563185257763f551e09929caf4
Aayush Kumar <falcon.bridge2000@gmail.com>
2022-05-03 18:06:59 +0530

    made the login form and routes for rendering login form and for the submission for it

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const passport = require("passport");
 
 const User = require("../models/user");
 const catchAsync = require("../utils/catchAsync");
@@ -23,6 +24,22 @@ router.post(
       res.redirect("register");
     }
   })
+);
+
+router.get("/login", (req, res) => {
+  res.render("users/login");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureFlash: true,
+    failureRedirect: "/login",
+  }),
+  (req, res) => {
+    req.flash("success", "welcome back!");
+    res.redirect("/campgrounds");
+  }
 );
 
 module.exports = router;
