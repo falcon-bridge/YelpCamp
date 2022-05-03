ff810e011daa30f12c5eff5c40900c2b7115d97e
Aayush Kumar <falcon.bridge2000@gmail.com>
2022-05-04 03:47:50 +0530

    prevented the unauthorized users fromediting or deleting the campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -65,6 +65,10 @@ router.get(
       req.flash("error", "Cannot find that campground!");
       return res.redirect("/campgrounds");
     }
+    if (!campground.author.equals(req.user._id)) {
+      req.flash("error", "You don't have permission to do that");
+      return res.redirect(`/campgrounds/${campground._id}`);
+    }
     res.render("campgrounds/edit", { campground });
   })
 );
@@ -75,7 +79,12 @@ router.put(
   validateCampground,
   catchAsync(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {
+    const campground = await Campground.findById(id);
+    if (!campground.author.equals(req.user._id)) {
+      req.flash("error", "You don't have permission to do that");
+      return res.redirect(`/campgrounds/${campground._id}`);
+    }
+    const camp = await Campground.findByIdAndUpdate(id, {
       ...req.body.campground,
     });
     req.flash("success", "Successfully updated campground!");
