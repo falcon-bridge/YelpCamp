1b433c143b03f1e23e286a138b7999fc6ea838d8
Aayush Kumar <falcon.bridge2000@gmail.com>
2022-05-04 05:30:15 +0530

    made sure that only authorized users can delete the reviews

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 const Campground = require("./models/campground");
+const Review = require("./models/review");
 
 const { campgroundSchema, reviewSchema } = require("./schemas.js");
 
@@ -41,4 +42,14 @@ module.exports.validateReview = (req, res, next) => {
   } else {
     next();
   }
+};
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+  const { id, reviewId } = req.params;
+  const review = await Review.findById(reviewId);
+  if (!review.author.equals(req.user._id)) {
+    req.flash("error", "You don't have permission to do that");
+    return res.redirect(`/campgrounds/${id}`);
+  }
+  next();
 };
