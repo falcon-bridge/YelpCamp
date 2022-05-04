852f02c33ed38810c0d08ff3c9d9b1c7333201f3
Aayush Kumar <falcon.bridge2000@gmail.com>
2022-05-04 21:40:51 +0530

    used the multer middleware to get the files uploaded and store them on a specified destination in our directory

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
 
 const campgrounds = require("../controllers/campgrounds");
 const catchAsync = require("../utils/catchAsync");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
 
+const upload = multer({ dest: "uploads/" });
+
 router
   .route("/")
   .get(catchAsync(campgrounds.index))
-  .post(
-    isLoggedIn,
-    validateCampground,
-    catchAsync(campgrounds.createCampground)
-  );
+  .post(upload.array("image"), (req, res) => {
+    console.log(req.body, req.files);
+    res.send("It worked");
+  });
+// .post(
+//   isLoggedIn,
+//   validateCampground,
+//   catchAsync(campgrounds.createCampground)
+// );
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
