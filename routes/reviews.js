20731f6ad3c1392b299c6bcc059ea4f94c441e23
Aayush Kumar <falcon.bridge2000@gmail.com>
2022-05-04 10:40:55 +0530

    added the reviews and users controllers in separate files

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,40 +1,19 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true }); //mergeparams set to true to get the id params we have in the app.js, otherwise they won't be taken here into consideration
 
-const Campground = require("../models/campground");
-const Review = require("../models/review");
+const reviews = require("../controllers/reviews");
 
 const catchAsync = require("../utils/catchAsync");
 
 const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
 
-router.post(
-  "/",
-  isLoggedIn,
-  validateReview,
-  catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    review.author = req.user._id;
-    await review.save();
-    await campground.save();
-    req.flash("success", "Created new review!");
-    res.redirect(`/campgrounds/${campground._id}`);
-  })
-);
+router.post("/", isLoggedIn, validateReview, catchAsync(reviews.createReview));
 
 router.delete(
   "/:reviewId",
   isLoggedIn,
   isReviewAuthor,
-  catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    req.flash("success", "Successfully deleted review");
-    res.redirect(`/campgrounds/${id}`);
-  })
+  catchAsync(reviews.deleteReview)
 );
 
 module.exports = router;
