afb888f1221cd92a7f03c2cc30bf73645afcc26b
Aayush Kumar <falcon.bridge2000@gmail.com>
2022-05-06 05:35:16 +0530

    chaned the seeds to contain distinct latitudes and logitudes

--- seeds/index.js
+++ seeds/index.js
@@ -32,7 +32,10 @@ const seedDB = async () => {
       price,
       geometry: {
         type: "Point",
-        coordinates: [72.877393, 19.07599],
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ],
       },
       images: [
         {
